3717cc2febf7859f7ea5414e88aefd5d190f5796
Михаил Казанджи <mkazanji@mobiledep.ru>
2022-05-16 18:41:10 +0300

    index full

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+
+  protected
+
+  def set_locale
+    # Remove inappropriate/unnecessary ones
+    I18n.locale = params[:locale] ||    # Request parameter
+      session[:locale] ||               # Current session
+      (current_user.preferred_locale if user_signed_in?) ||  # Model saved configuration
+      extract_locale_from_accept_language_header ||          # Language header - browser config
+      I18n.default_locale               # Set in your config files, english by super-default
+  end
+
+  # Extract language from request header
+  def extract_locale_from_accept_language_header
+    if request.env['HTTP_ACCEPT_LANGUAGE']
+      lg = request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first.to_sym
+      lg.in?([:en, YOUR_AVAILABLE_LANGUAGES]) ? lg : nil
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'work/index'
+  get 'work/choose_theme'
+  get 'work/display_theme'
   resources :themes
   resources :images
   resources :values
